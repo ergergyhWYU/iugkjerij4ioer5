3cc361227148fba0c89043e2fe6f607260dfd1b0
ergergyhWYU <hmm063585@gmail.com>
2026-06-15 01:20:58 +0530

    Update Perrmission.lua

--- Perrmission.lua
+++ Perrmission.lua
@@ -1,1 +1,15 @@
-
+return {
+    -- Maintenance Toggles for each game (true = Open/Active, false = Closed/Maintenance)
+    GamesStatus = {
+        ["Grow A Garden 2"] = true,
+        ["Blox Fruits"]      = true,
+        ["Mini War"]        = true,
+        ["Ultra Unfair"]    = true
+    },
+    
+    -- List of usernames allowed to bypass maintenance and run the scripts
+    SafePlayers = {
+        "Sanjeev",
+        -- Add other tester usernames here (separated by commas)
+    }
+}
