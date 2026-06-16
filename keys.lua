6f4a0f5f547692bd864ddbd331469e0de288b934
ergergyhWYU <hmm063585@gmail.com>
2026-06-17 01:26:25 +0530

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,1 +1,8 @@
-
+return {
+    KeyCount = 3, -- Change this to update the display in the Home tab
+    Keys = {
+        "KEY_LAZYDEVSzAbCdEfGhIjKlMnOpQrStUvWxYzAb",
+        "KEY_UPDATESOONxYzA2bCdEfGhIjKlMnOpQrStUvW",
+        "KEY_DORACAKEISREALgHjKmNqRtUvWxYzAbCdEfG",
+    }
+}
