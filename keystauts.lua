b81053d47fe2b97ffcf7c84ff46c6e4c18c7bbde
ergergyhWYU <hmm063585@gmail.com>
2026-06-17 11:39:19 +0530

    Update keystauts.lua

--- keystauts.lua
+++ keystauts.lua
@@ -1,1 +1,5 @@
+return {
+    RestockNote = "WE WILL LET YOU KNOW WHEN KEYS RESTOCK,AND YOU KNOW YOU CAN REEDEM THEM DIRECT FROM REPLACING KEY IN WORKSPACE",
+    KeysRedeemed = 0 -- Update this number whenever a key is redeemed!
+}
 
