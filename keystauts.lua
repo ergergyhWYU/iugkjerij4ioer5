17d4cd24fe9b66193df446a599157f7bd46f91e2
ergergyhWYU <hmm063585@gmail.com>
2026-06-26 22:28:38 +0530

    Update keystauts.lua

--- keystauts.lua
+++ keystauts.lua
@@ -1,5 +1,5 @@
 return {
-    RestockNote = "WE WILL LET YOU KNOW WHEN KEYS RESTOCK,FIXED AUTO STEAL AND STEAL LARGEST IF NOT WORK REEXCUTE IN 5 MIN",
-    KeysRedeemed = 1 -- Update this number whenever a key is redeemed!
+    RestockNote = "stoped",
+    KeysRedeemed = 0
 }
 
